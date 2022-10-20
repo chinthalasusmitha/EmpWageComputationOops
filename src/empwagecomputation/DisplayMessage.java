cf82f58db9059d2b8de8ec21abbf17f61979ad96
susmitha <susmithareddychinthala024@gmail.com>
2022-10-20 11:40:21 +0530

    ADDED: Welcome to Emp Wage

---
+++ src/empwagecomputation/DisplayMessage.java
@@ -0,0 +1,7 @@
+package empwagecomputation;
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
